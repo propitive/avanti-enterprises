2d3916e7137bc88e5540747dbfba755f8bbc10b3
Propitive <propitive@gmail.com>
2023-10-02 20:29:34 -0500

    restructured files

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import avantiLogo from "../images/avantiLogo.jpg";
+
+function Header() {
+  return (
+    <header className="header">
+      <img src={avantiLogo} />
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,7 @@
+import Header from "../App/Header/Header";
+
+function Main() {
+  return <Header />;
+}
+
+export default Main;
