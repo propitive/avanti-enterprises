5d0512955b03cbf8d19909ee5ee39ba0fe23fc03
Propitive <propitive@gmail.com>
2023-10-03 07:58:54 -0500

    outline for header complete

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,21 @@
 import avantiLogo from "../../images/avantiLogo.jpg";
+import iconInstagram from "../../images/icons/instagram.svg";
+import iconFacebook from "../../images/icons/facebook.svg";
 
 function Header() {
   return (
     <header className="header">
-      <img src={avantiLogo} />
+      <img className="header__image" src={avantiLogo} />
+      <ul className="header__navbar">
+        <li className="header__list-item">Kitchen Cabinets</li>
+        <li className="header__list-item">Bathroom Cabinets</li>
+        <li className="header__list-item">Vinyl Planking</li>
+        <li className="header__list-item">About Us</li>
+      </ul>
+      <div className="header__icons">
+        <img className="header__icon" src={iconInstagram} />
+        <img className="header__icon" src={iconFacebook} />
+      </div>
     </header>
   );
 }
