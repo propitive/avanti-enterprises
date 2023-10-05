86dc5246abc4af0637d221e1b818a19a2a9600a9
Jose M. Maldonado <jmaldonado@associa.us>
2023-10-05 18:16:56 -0500

    footer component needs work

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,3 +1,8 @@
 function footer() {
-  return <footer className="footer"></footer>;
+  return (
+    <footer className="footer">
+      <div className="footer__links"></div>
+      <div className="footer__email-list"></div>
+    </footer>
+  );
 }
