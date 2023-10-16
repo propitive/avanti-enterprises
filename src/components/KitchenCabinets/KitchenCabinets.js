034305af52274efad231be1c7e6199200814401c
Jose M. Maldonado <jmaldonado@associa.us>
2023-10-16 15:11:36 -0500

    started prototype for kitchen cabinet page

--- src/components/KitchenCabinets/KitchenCabinets.js
+++ src/components/KitchenCabinets/KitchenCabinets.js
@@ -5,10 +5,26 @@ function KitchenCabinets() {
   return (
     <>
       <Header />
-      <div></div>
+      <div className="k-cabinets">
+        <div className="k-cabinets__search">
+          <h3 className="k-cabinets__trigger">Finish</h3>
+          <div className="k-cabinets__menu-finish">
+            <ul></ul>
+          </div>
+          <h3 className="k-cabinets__trigger">Design</h3>
+          <h3 className="k-cabinets__trigger">Width</h3>
+          <h3 className="k-cabinets__trigger">Height</h3>
+          <h3 className="k-cabinets__trigger">Color</h3>
+        </div>
+        <div className="k-cabinets__products"></div>
+      </div>
       <Footer />
     </>
   );
+}
+
+function DropdownItem(props) {
+  return <li></li>;
 }
 
 export default KitchenCabinets;
