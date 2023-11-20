0432be36dfd2097f37483f35c5a38712118952ec
Propitive <propitive@gmail.com>
2023-11-20 16:04:41 -0600

    benefits are now fully responsive

--- src/components/Benefits/Benefits.js
+++ src/components/Benefits/Benefits.js
@@ -5,39 +5,31 @@ function Benefits() {
         <h2 className="benefits__header">What will Avanti mean to you?</h2>
         <div className="benefits__text-container">
           <div className="benefits__column">
-            <h3 className="benefits__column-title">Lorem ipsum dolor</h3>
+            <h3 className="benefits__column-title">Commitment To Quality</h3>
             <p className="benefits__column-paragraph">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Our commitment to quality is unwavering. Each cabinet installation
+              is a testament to our meticulous attention to detail and
+              dedication to using only the finest materials. Your satisfaction
+              is not just a goal for us; it's a guarantee.
             </p>
           </div>
           <div className="benefits__column">
-            <h3 className="benefits__column-title">Lorem ipsum dolor</h3>
+            <h3 className="benefits__column-title">Customer Satisfaction</h3>
             <p className="benefits__column-paragraph">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Communication is a priority, ensuring you are informed and
+              involved every step of the way. Our commitment doesn't end with
+              the completion of the project; we stand by our work, offering
+              after-sales support to address any concerns.
             </p>
           </div>
           <div className="benefits__column">
-            <h3 className="benefits__column-title">Lorem ipsum dolor</h3>
+            <h3 className="benefits__column-title">Timeliness </h3>
             <p className="benefits__column-paragraph">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              We understand the importance of deadlines, and we take pride in
+              our ability to complete projects on schedule without compromising
+              on quality. Your time is valuable, and we ensure that every
+              installation is a seamless, hassle-free experience from start to
+              finish.
             </p>
           </div>
         </div>
--- src/components/EmailListForm/EmailListForm.js
+++ src/components/EmailListForm/EmailListForm.js
@@ -1,6 +1,30 @@
+import React, { useRef } from "react";
 import BookOnlineButton from "../BookOnlineButton/BookOnlineButton";
 
 function EmailListForm() {
+  const form = useRef();
+
+  // const sendEmail = (e) => {
+  //   e.preventDefault();
+
+  //   emailjs
+  //     .sendForm(
+  //       "service_svf8kqt",
+  //       "template_v5yhpim",
+  //       form.current,
+  //       "jGNwN29o5MpAtqpNz"
+  //     )
+  //     .then(
+  //       (result) => {
+  //         console.log(result.text);
+  //       },
+  //       (error) => {
+  //         console.log(error.text);
+  //       }
+  //     );
+  //   e.target.reset();
+  // };
+
   return (
     <div className="email-list">
       <h3 className="email-list__header">JOIN OUR MAILING LIST</h3>
