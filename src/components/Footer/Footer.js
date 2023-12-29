82cd8c7240d31122567b940e32dac520049e155e
Propitive <propitive@gmail.com>
2023-12-29 15:07:13 -0600

    added links to the navigation items in the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,5 @@
 import EmailListForm from "../EmailListForm/EmailListForm";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
@@ -6,13 +7,37 @@ function Footer() {
       <div className="footer__links">
         <div className="footer__column">
           <h4 className="footer__column-title">Navigation</h4>
-          <p className="footer__column-link">Kitchen Cabinets</p>
+          <Link
+            to="/services/kitchen-cabinets"
+            style={{ textDecoration: "none", color: "#f5f0f0" }}
+          >
+            <p className="footer__column-link">Kitchen Cabinets</p>
+          </Link>
+          <Link
+            to="/services/bathroom-cabinets"
+            style={{ textDecoration: "none", color: "#f5f0f0" }}
+          >
+            <p className="footer__column-link">Bathroom Cabinets</p>
+          </Link>
+          <Link
+            to="/services/vinyl-planking"
+            style={{ textDecoration: "none", color: "#f5f0f0" }}
+          >
+            <p className="footer__column-link">Vinyl Planking</p>
+          </Link>
+
+          {/* <p className="footer__column-link">Kitchen Cabinets</p>
           <p className="footer__column-link">Bathroom Cabinets</p>
-          <p className="footer__column-link">Vinyl Planking</p>
+          <p className="footer__column-link">Vinyl Planking</p> */}
         </div>
         <div className="footer__column">
           <h4 className="footer__column-title">Our Company</h4>
-          <p className="footer__column-link">About</p>
+          <Link
+            to="/about-us"
+            style={{ textDecoration: "none", color: "#f5f0f0" }}
+          >
+            <p className="footer__column-link">About</p>
+          </Link>
         </div>
       </div>
       <EmailListForm />
